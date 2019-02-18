af304d6463cc11061444e2f7911c2a4f6616498d
Matthew Lockdall <mlockdall26@gmail.com>
2019-02-17 19:52:54 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("Up and Running");
+
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log("user flipped" + " " + cardOne + " and " + cardTwo);
