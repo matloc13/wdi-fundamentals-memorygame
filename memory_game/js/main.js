6fa07b6cae5221b52a210677765a1d8d6da093fc
Matthew Lockdall <mlockdall26@gmail.com>
2019-02-20 20:54:27 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,23 @@
 console.log("Up and Running");
 
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = []
 
-console.log("user flipped" + " " + cardOne + " and " + cardTwo);
+var cardOne = cards[0];
+var cardTwo = cards[3];
+var cardThree = cards[2];
+var cardFour = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped" + " " + cardOne);
+console.log("user flipped" + " " + cardTwo);
+
+if(cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }else{
+    alert("Sorry Try Again");
+  }
+}
