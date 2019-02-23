51f05209212a023ab5daac27a8676d6d2491c2a3
Matthew Lockdall <mlockdall26@gmail.com>
2019-02-23 17:21:52 -0600

    created game logic ie adding objects to array

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and Running");
 
-var cards = ['queen','queen','king','king'];
+//var cards = ['queen','queen','king','king'];
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank:'queen',
+    suit:'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank:'king',
+    suit:'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank:'king',
+    suit:'diamonds',
+    cardImage:'images/king-of-diamonds.png'
+  }
+];
 var cardsInPlay = []
 
 var cardOne = cards[0];
@@ -9,7 +31,7 @@ var cardThree = cards[2];
 var cardFour = cards[1];
 
 function checkForMatch(){
-    if(cardsInPlay[0] === cardsInPlay[1]){
+    if (cardsInPlay[0] === cardsInPlay[1]){
       alert("You found a match!");
     }else{
       alert("Sorry Try Again");
@@ -17,9 +39,12 @@ function checkForMatch(){
 };
 
 function flipCard(cardId){
+
   checkForMatch();
-  console.log("You flipped a" + " " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+
+  console.log("You flipped a" + " " + cards[cardId].rank + " " + "of" + " " + cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
 
 };
 
