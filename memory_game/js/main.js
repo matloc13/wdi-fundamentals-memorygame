780b640aab95bbe397fd1181da4f25e61ab96f27
Matthew Lockdall <mlockdall26@gmail.com>
2019-02-25 23:00:25 -0600

    still having trouble on confirming matches

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,4 @@
 console.log("Up and Running");
-
 //var cards = ['queen','queen','king','king'];
 var cards = [
   {
@@ -23,30 +22,61 @@ var cards = [
     cardImage:'images/king-of-diamonds.png'
   }
 ];
+
 var cardsInPlay = []
 
 var cardOne = cards[0];
-var cardTwo = cards[3];
-var cardThree = cards[2];
-var cardFour = cards[1];
+var cardTwo = cards[1];
+var cardThree = cards[3];
+var cardFour = cards[2];
+
 
 function checkForMatch(){
-    if (cardsInPlay[0] === cardsInPlay[1]){
+
+if (cardsInPlay.length >= 1){
+  if(cardsInPlay[0] === cardsInPlay[1]){
       alert("You found a match!");
+    }else if(cardsInPlay.length > 1){
+      alert("Please RESET and try again.");
     }else{
       alert("Sorry Try Again");
     }
+  }
 };
 
-function flipCard(cardId){
 
+function flipCard(){
+
+var cardId = this.getAttribute('data-id');
+this.setAttribute('src', cards[cardId].cardImage);
   checkForMatch();
 
   console.log("You flipped a" + " " + cards[cardId].rank + " " + "of" + " " + cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
-
 };
 
-flipCard(0);
-flipCard(2);
+
+
+function createBoard(){
+
+  for (i = 0; i <= cards.length-1; i++ ){
+    var cardElement = document.createElement('img');// creates empty img element
+      cardElement.setAttribute('src', 'images/back.png'); // gives img source
+
+      cardElement.setAttribute('data-id', i);
+      cardElement.addEventListener('click', flipCard); // listener to run flip card
+    var imgDiv = document.getElementById('game-board');//append new img to div
+    imgDiv.appendChild(cardElement);
+
+  }
+};
+
+createBoard();
+
+function resetPage(){
+  location.reload();
+};
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetPage);
